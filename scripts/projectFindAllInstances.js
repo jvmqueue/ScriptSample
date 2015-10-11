6230e356087dd96e077cdf648a46a423513bccea
jvmqueue <wheels0090@yahoo.com>
2015-10-11 13:43:00 -0400

    implement methods

--- scripts/projectFindAllInstances.js
+++ scripts/projectFindAllInstances.js
@@ -1,16 +1,48 @@
 define([], function(){
+    var w = window, d = document;
     var mInstance = null;
     var _Project = function(){
-        console.group('_PROJECT FIND ALL INSTANCES');
-            console.log(':\t', 'Reached');
-           console.groupEnd(); 
+        this.paragraph = null;
     };
+
+    var util = {
+        appendToDom:function(options){
+            frag = d.createDocumentFragment();
+            var nodeText = d.createTextNode(options.paramStr);
+            var nodeNew = d.createElement(options.paramNodeName);
+            nodeNew.setAttribute('class', 'jsBold');
+            nodeNew.appendChild(nodeText);
+            frag.appendChild(nodeNew);
+            options.$paramNodeExist.append(frag);
+        }
+    }; // End util
+
     _Project.prototype = {
-         generateLongParagraph:function(){},
-         findInstances:function(){},
+         generateLongParagraph:function(){
+            var strParagraph = 'The oldest and, and classical Greek and Latin writing had little or no space between words, and could be written in boustrophedon (alternating directions). Over time, text direction (left to right) became standardized, and word dividers and terminal punctuation became common.';
+            var frag = d.createDocumentFragment();            
+            var nodeText = d.createTextNode(strParagraph);
+            var nodeNew = d.createElement('div');
+            this.paragraph = strParagraph;
+            nodeNew.appendChild(nodeText);
+            frag.appendChild(nodeNew);
+            return frag;
+         },
+         findInstances:function(paramWordToFind){
+            var strWordToFind = paramWordToFind;
+            var regEx = new RegExp('(\\s'+ strWordToFind +'\\s)|(\\s'+ strWordToFind +',)', 'ig');
+            var intNumMatches = this.paragraph.match(regEx).length;
+            return {intNumOfMatches:intNumMatches, strWordMatched:strWordToFind};
+         },
          most:function(){},
          printSolution:function($paramNode){
-            $paramNode.html('Reached find all instances');
+            var $nodeExist = $paramNode;
+            var frag = this.generateLongParagraph();
+            $nodeExist.html('Solution: Find All Instances');
+            $nodeExist.append(frag);
+            var objMatches = this.findInstances('and');
+            var strText = 'Found the word "' + objMatches.strWordMatched + '" in paragraph ' + objMatches.intNumOfMatches + ' times';
+            util.appendToDom({paramStr:strText, paramNodeName:'p', $paramNodeExist:$nodeExist});    
          }         
     };
     var _getInstance = function(){ // singleton, can be instantiated once
